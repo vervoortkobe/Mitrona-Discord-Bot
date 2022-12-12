aac8e3aa0f443fc077fc1e0107f3645bcb7af56e
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2022-12-12 15:05:00 +0100

    Create dummy.js

---
+++ commands/dummy.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (client, message, args) => {
+
+    console.log("dummy");
+  }
+
+  module.exports.help = {
+    name: "dummy",
+    aliases: ["none"],
+    category: "none"
+}
