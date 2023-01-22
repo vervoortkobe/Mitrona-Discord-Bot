83990bfec45e03494c67c9d26d1655b78af9435d
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2023-01-22 14:48:09 +0100

    Update interactionCreate.js

--- devents/interactionCreate.js
+++ devents/interactionCreate.js
@@ -10,84 +10,16 @@ module.exports.run = async (client, interaction) => {
       }
     }
     
+    if(interaction.user.bot) return;
+    if(interaction.channel.type === "DM") return;
+    
     if(!interaction.isChatInputCommand()) return;
     if(!interaction.isCommand()) return;
-    if(interaction.author.bot) return;
-    if(interaction.channel.type === "DM") return;
 
     const { commandName, options } = interaction;
 
-//ANNOUNCE
-    if(commandName === "announce") {
-      let intcommandfile = require("./intcommands/announce.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//AUTOROLE
-    if(commandName === "autorole") {
-      let intcommandfile = require("./intcommands/autorole.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//CITIZEN
-    if(commandName === "citizen") {
-      let intcommandfile = require("./intcommands/citizen.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//CLEAR
-    if(commandName === "clear") {
-      let intcommandfile = require("./intcommands/clear.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//DASHBOARD
-    if(commandName === "dashboard") {
-      let intcommandfile = require("./intcommands/dashboard.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//EVAL
-    if(commandName === "eval") {
-      let intcommandfile = require("./intcommands/eval.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//GCANCEL
-    if(commandName === "gcancel") {
-      let intcommandfile = require("./intcommands/gcancel.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//GCHECK
-    if(commandName === "gcheck") {
-      let intcommandfile = require("./intcommands/gcheck.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//GEND
-    if(commandName === "gend") {
-      let intcommandfile = require("./intcommands/gend.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//GIVEAWAY
-    if(commandName === "giveaway") {
-      let intcommandfile = require("./intcommands/giveaway.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//GREROLL
-    if(commandName === "greroll") {
-      let intcommandfile = require("./intcommands/greroll.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
-
-//UNCITIZEN
-    if(commandName === "uncitizen") {
-      let intcommandfile = require("./intcommands/uncitizen.js");
-      if(intcommandfile) intcommandfile.run(client, interaction, commandName, options, fs);
-    }
+    let intcommandfile = client.intcommands.get(commandName);
+    if(intcommandfile) intcommandfile.run(client, interaction);
   }
 
   module.exports.help = {
