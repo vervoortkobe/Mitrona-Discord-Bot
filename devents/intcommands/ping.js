0d52b927a4dea4738a02095d0d2267def6bba050
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2023-01-23 18:08:18 +0100

    Update ping.js

--- devents/intcommands/ping.js
+++ devents/intcommands/ping.js
@@ -7,15 +7,16 @@ module.exports.run = async (client, interaction) => {
     .setColor(0x03a9f4)
     .setDescription(`🔍 **|** ***Calculating...***`)
 
-    const m = await interaction.reply({ embeds: [ mEmbed ], ephemeral: true });
-
+    const m = Date.now();
+    await interaction.reply({ embeds: [ mEmbed ], ephemeral: true });
+  
     const pingEmbed = new Discord.EmbedBuilder()
     .setColor(0x00ff00)
     .setTitle(`🏓 PING`)
     .setThumbnail(client.user.displayAvatarURL())
-    .setDescription(`***Pong!***\n> 💬 **| Message Latency:** \`${m.createdTimestamp - message.createdTimestamp}\`**ms**
+    .setDescription(`***Pong!***\n> 💬 **| Message Latency:** \`${Date.now() - m}\`**ms**
     > 🤖 **| Discord API Heartbeat:** \`${Math.round(client.ws.ping)}\`**ms**`)
-    m.edit({ embeds: [ pingEmbed ], ephemeral: true });
+    interaction.editReply({ embeds: [ pingEmbed ], ephemeral: true });
   }
 
   module.exports.help = {
